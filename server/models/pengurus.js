7f6f97b724a988e3fb7e757074e87d050e9cf164
Pragistyo <new.SOS.hope@outlook.com>
2018-02-05 19:43:30 +0700

    fixing pengurus model

--- server/models/pengurus.js
+++ server/models/pengurus.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema
 var pengurusSchema = new Schema({
     pengurusNama: { type: String, required: true },
     pengurusKTP: { type: String, required: true },
-    pengurusEmail: { type: String, required: true },
-    pengurusKepemilikan: { type: String, required: true },
+    pengurusEmail: { type: String, required: true }
 }, { timestamps: true })
 
 var pengurus = mongoose.model('pengurus', pengurusSchema)
